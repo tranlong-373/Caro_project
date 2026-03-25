6c77352d349c1400ec85018a4ce184f448a0caff
tranlong-373 <pika297006@gmail.com>
2026-03-25 23:39:04 +0700

    Delete caro_zip directory

--- caro_zip/MenuNavigation.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-namespace caro {
-
-    int WrapSelectionIndex(int currentIndex, int delta, int itemCount);
-    int ClampValue(int value, int minValue, int maxValue);
-
-} // namespace caro
